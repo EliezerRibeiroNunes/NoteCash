4173d498d07159ea15ff53a27c71af14eae9b6cb
Eliezer RN <eliezerrnunes@gmail.com>
2022-08-05 17:52:42 -0300

    Funcionalidade de editar e troca de página

--- lib/ui/edit.dart
+++ lib/ui/edit.dart
@@ -1,13 +1,14 @@
+import 'dart:io';
+
 import 'package:app/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 import '../helpers/controller.dart';
 
 class RegistryEdit extends StatefulWidget {
- final Contact contact;
+  final Contact contact;
 
   const RegistryEdit({Key key, this.contact}) : super(key: key);
-
 
   @override
   createState() => _RegistryEditState();
@@ -28,7 +29,7 @@ class _RegistryEditState extends State<RegistryEdit> {
   void initState() {
     super.initState();
 
-    if (widget.contact != null ) {
+    if (widget.contact == null) {
       _editedContact = Contact();
     } else {
       _editedContact = Contact.fromMap(widget.contact.toMap());
@@ -44,19 +45,17 @@ class _RegistryEditState extends State<RegistryEdit> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: const Text('CashNote'),
+        title: const Text('NoteCash'),
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          if(_editedContact.name != null && _editedContact.name.isNotEmpty){
+          if (_editedContact.name != null && _editedContact.name.isNotEmpty) {
             Navigator.pop(context, _editedContact);
-          }else {
-                FocusScope.of(context).requestFocus(_nameFocus);
+          } else {
+            FocusScope.of(context).requestFocus(_nameFocus);
           }
-
         },
-
         backgroundColor: Colors.blue,
         child: const Icon(Icons.save),
       ),
@@ -64,80 +63,53 @@ class _RegistryEditState extends State<RegistryEdit> {
         padding: const EdgeInsets.all(10.0),
         child: Column(
           children: <Widget>[
-            //GestureDetector(
-            //child: Container(
-            //width: 140.0,
-            //height:  140.0,
-            //decoration: BoxDecoration(
-            //shape: BoxShape.circle,
-            //image: DecorationImage(
-            //image: _editedContact.img != null ?
-            // FileImage(File(_editedContact.img)) :
-            //const AssetImage("assets/images/comprovante.png") as ImageProvider
 
-            TextField(
-              controller: _nameController,
-              focusNode: _nameFocus,
-              decoration: const InputDecoration(labelText: "Nome"),
-              onChanged: (text) {
-                _registryEdited = true;
-                setState(() {
-                  _editedContact.name = text;
-                });
-              },
-            ),
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(labelText: "Email"),
-              onChanged: (text) {
-                _registryEdited = true;
+                TextField(
+                  controller: _nameController,
+                  focusNode: _nameFocus,
+                  decoration: const InputDecoration(labelText: "Nome"),
+                  onChanged: (text) {
+                    _registryEdited = true;
+                    setState(() {
+                      _editedContact.name = text;
+                    });
+                  },
+                ),
+                TextField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(labelText: "Email"),
+                  onChanged: (text) {
+                    _registryEdited = true;
 
-                _editedContact.email = text;
-              },
-              keyboardType: TextInputType.emailAddress,
-            ),
+                    _editedContact.email = text;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                TextField(
+                  controller: _phoneController,
+                  decoration: const InputDecoration(labelText: "Valor"),
+                  onChanged: (text) {
+                    _registryEdited = true;
+                    _editedContact.phone = text;
+                  },
+                  keyboardType: TextInputType.number,
+                ),
 
-            TextField(
-              controller: _phoneController,
-              decoration: const InputDecoration(labelText: "Valor"),
-              onChanged: (text) {
-                _registryEdited = true;
-                _editedContact.phone = text;
-              },
-              keyboardType: TextInputType.number,
-            ),
-          ],
-        ),
-      ),
-
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.black,
-              ),
-              child: Text(
-                'CashNote',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
+            Text("Adicionar comprovante"),
+            GestureDetector(
+              child: Container(
+                width: 140.0,
+                height: 140.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: _editedContact.img != null ?
+                      FileImage(File(_editedContact.img)) :
+                      const AssetImage("assets/images/comprovante.png")
+                  ),
                 ),
               ),
             ),
-            ListTile(
-              leading: Icon(Icons.message),
-              title: Text('Messages'),
-            ),
-            ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text('Profile'),
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-            ),
+
           ],
         ),
       ),
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:app/helpers/controller.dart';
 import 'package:app/ui/edit.dart';
 import 'package:flutter/material.dart';
@@ -10,17 +8,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   ContactHelper helper = ContactHelper();
 
   // ignore: deprecated_member_use
   List<Contact> contacts = List();
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-   _getAllRegistries();
-
+    _getAllRegistries();
   }
 
   @override
@@ -28,25 +24,22 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: const Text('CashNote'),
+        title: const Text('NoteCash'),
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            _showRegistry();
-          },
+        onPressed: () {
+          _showRegistry();
+        },
         backgroundColor: Colors.green,
         child: const Icon(Icons.add),
       ),
       body: ListView.builder(
-       padding: const EdgeInsets.all(10.0),
-        itemCount: contacts.length,
-        itemBuilder: (context, index) {
-            return _registryList(context, index);
-        }
-
-
-          ),
+          padding: const EdgeInsets.all(10.0),
+          itemCount: contacts.length,
+          itemBuilder: (context, index) {
+            return _registryList(context, index) ;
+          }),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -80,64 +73,63 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-  Widget _registryList(BuildContext context, int index){
-        return GestureDetector(
-          child: Card(
-            child: Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Row(
+
+  Widget _registryList(BuildContext context, int index) {
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-
-                    Padding(
-                      padding: EdgeInsets.only(left: 10.0),
-                      child: Row(
-                        children: <Widget>[
-
-                          Text(contacts[index].name ?? "",
-                          style: TextStyle(fontSize: 12.0,
-                            fontWeight: FontWeight.bold
-                            ),
-                          ),
-                          Text(contacts[index].phone ?? "",
-                        style: TextStyle(fontSize: 12.0,
-                            fontWeight: FontWeight.normal
-                        ),
-                        ),
-                        ],
-                      ),
+                    Text(
+                      contacts[index].name ?? "",
+                      style: TextStyle(
+                          fontSize: 12.0, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      contacts[index].phone ?? "",
+                      style: TextStyle(
+                          fontSize: 12.0, fontWeight: FontWeight.normal),
                     ),
                   ],
                 ),
-            ),
-
+              ),
+            ],
           ),
-          onTap: (){
-            _showRegistry(contact: contacts[index]);
-          },
-        );
-
+        ),
+      ),
+      onTap: () {
+        _showRegistry(contact: contacts[index]);
+      },
+    );
   }
 
   void _showRegistry({Contact contact}) async {
-    final recRegistry = await Navigator.push(context,
-        MaterialPageRoute(builder: (context) => RegistryEdit(contact: Contact()),)
-    );
-    if (recRegistry != null){
-      if(contact != null){
+   final recRegistry = await Navigator.push(
+       context,
+        MaterialPageRoute(
+          builder: (context) => RegistryEdit(contact: contact,))
+   );
+    if (recRegistry != null) {
+      if (contact != null) {
         await helper.updateContact(recRegistry);
-
-      }else {
+      } else {
         await helper.saveContact(recRegistry);
       }
       _getAllRegistries();
     }
   }
- void _getAllRegistries(){
-   helper.getAllContacts().then((list){
-     setState (() {
-       contacts = list as List<Contact>;
-     });
-   });
- }
 
+  void _getAllRegistries() {
+    helper.getAllContacts().then((list) {
+      setState(() {
+        contacts = list as List<Contact>;
+      });
+    });
+  }
 }
